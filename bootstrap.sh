3cc142015df182b17a4c882e977ebebc5dde6447
Shyam Upadhyay <shyamupa@gmail.com>
2014-01-03 20:14:27 -0600

    removed bootstrap.sh

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-MYPATH=`fortune -f 2>&1 | head -1 | cut -d' ' -f2`
-echo $MYPATH
-files=`ls -1 | egrep -v '\.sh|makefile|README'`
-for file in $files
-do
-    cp $file $MYPATH
-    echo "Copied file $file to destination"
-done
